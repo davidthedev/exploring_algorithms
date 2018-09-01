a31e23254d0a7f834703725efdd59bf7802c958f
David C <davidthedev@users.noreply.github.com>
2018-09-01 11:51:02 +0100

    Comment array chunks algorithm

--- arrayChunking/index.js
+++ arrayChunking/index.js
@@ -1,9 +1,13 @@
 function chunk(array, size) {
-  let newArr = [];
+  const newArr = [];
 
   for (let val of array) {
+    // get last element in the array (passed by reference)
     const last = newArr[newArr.length - 1];
 
+    // if there isn't a last element
+    // or length of last array equals to the size of chunk that we passed in
+    // push into the new array, otherwise keep on pushing into the last array
     if (!last || last.length === size) {
       newArr.push([val]);
     } else {
@@ -11,7 +15,7 @@ function chunk(array, size) {
     }
   }
 
-    return newArr;
+  return newArr;
 }
 
 module.exports = chunk;
